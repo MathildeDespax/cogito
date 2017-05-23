fd99aaf539c47f3ca4f65f4d1e53c0fbf08ad13d
Gregory Debargue <gdebargue@gmail.com>
2017-05-23 16:19:17 +0200

    validations+associations done

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,3 +1,12 @@
 class Classroom < ApplicationRecord
-  belongs_to :user
+  has_many :user_classrooms
+
+  has_many :students, through: :user_classrooms, source: :user
+
+  has_many :shares
+  has_many :quizzs, through: :shares
+
+  belongs_to :teacher, class_name: "User", foreign_key: :user_id
+
+  validates :name, presence: true
 end
