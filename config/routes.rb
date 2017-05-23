b4c20cbff052088b6e42dada2964f908ae2b8816
Bastien Pinot <bastienpinot@hotmail.fr>
2017-05-23 17:06:42 +0200

    on a fait des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :users do
+    resources :quizzs do
+      resources :cards
+    end
+    resources :classrooms
+    resources :student_evaluations, only: [:index, :create, :show]
+  end
+  resources :shares, only: [:new, :create]
 end
