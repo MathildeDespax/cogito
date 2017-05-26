467f40b9ef34d7b8a96646fabe3b759d950854c3
Mathilde Despax <mat.despax@gmail.com>
2017-05-26 11:22:58 +0200

    user story until share

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,12 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  def teacher?
+    !quizzs.empty?
+  end
+
+  def student?
+    !user_classrooms.empty?
+  end
+
 end
