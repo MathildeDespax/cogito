40f1bd25a40cf52e665982e12bf00ecec94c8b74
Mathilde Despax <mat.despax@gmail.com>
2017-05-26 16:26:26 +0200

    error syntax solved

--- app/controllers/students/student_evaluations_controller.rb
+++ app/controllers/students/student_evaluations_controller.rb
@@ -9,14 +9,15 @@ class StudentEvaluationsController < ApplicationController
     @card = Card.find(params[:card_id])
     @student_evaluation = StudentEvaluation.new(user: @student, card: @card)
     if @student_evaluation.save
-    respond_to do |format|
-       format.html { redirect_to students_quizz_path(@card.quizz) }
-       format.js  # <-- will render `app/views/reviews/create.js.erb`
-     end
-   else
-     respond_to do |format|
-       format.html { render 'students/quizzs/show' }
-       format.js  # <-- idem
-     end
-   end
+      respond_to do |format|
+         format.html { redirect_to students_quizz_path(@card.quizz) }
+         format.js  # <-- will render `app/views/reviews/create.js.erb`
+       end
+     else
+       respond_to do |format|
+         format.html { render 'students/quizzs/show' }
+         format.js  # <-- idem
+       end
+    end
+  end
  end
