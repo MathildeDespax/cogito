23a050b84168b26a1631f172c57eb9cc891022d2
Bastien Pinot <bastienpinot@hotmail.fr>
2017-06-01 16:01:48 +0200

    share link for each quiz with modal and timer on flashes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :quizzs, shallow: true do
+    resources :shares, only: [:new, :create]
     resources :cards do
       resources :student_evaluations, only: [:index, :create, :show]
     end
   end
 
   resources :classrooms
-  resources :shares, only: [:new, :create]
 
   namespace :students do
     resources :quizzs, only: [:show, :index]
